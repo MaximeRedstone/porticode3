4dd2cee977557ad48c4c10409f3a5a4efd48f881
dbhgold <dbhgold@gmail.com>
2018-11-17 15:20:10 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,3 @@
-from numpy import math #Importing numerical Python
-import ipywidgets as widgets #Importing widgets
-d = widgets.Dropdown(options=['s', 'u', 'v', 'a', 't'], value='s')
-display
-d
+import cgi
+form = cgi.FieldStroage()
+searchterm = form.getvalue('searchbox')
